a0f82b632001669865204418c61d2d39dc684e97
Alex Lübeck <ndsvw@gmx.de>
2020-03-14 15:21:39 +0100

    third try

--- main.py
+++ main.py
@@ -130,3 +130,48 @@ def maxHeightTry2(d1, d2, d3):
                 max_height[x][y] = max_tmp
     return max_height[max_dimension][max_dimension]
 
+
+def maxHeightTry3(d1, d2, d3):
+    """
+    A method that calculates largest possible tower height of given boxes.
+    Problem description: https://practice.geeksforgeeks.org/problems/box-stacking/1
+        time complexity: O(n^2)
+        space complexity: O(n)
+
+    Parameters
+    ----------
+    d1 : int[]
+        a list of int values representing the 1st dimension of a / multiple 3d-box(es)
+    d2 : int[]
+        a list of int values representing the 2nd dimension of a / multiple 3d-box(es)
+    d3 : int[]
+        a list of int values representing the 3rd dimension of a / multiple 3d-box(es)
+
+    Returns
+    -------
+    x  : int
+        the largest possible tower height
+    """
+
+    assert(len(d1) >= 1)
+    assert(len(d1) == len(d2) == len(d3))
+
+    boxes = zip(d1, d2, d3)
+    boxes = map(sorted, boxes)
+    boxes = sorted(boxes, key=lambda e: (e[0], e[1], e[2]))
+    boxes = boxes + list(map(lambda e: [e[1], e[2], e[0]], boxes))
+    boxes = boxes + list(map(lambda e: [e[0], e[2], e[1]], boxes))
+    boxes = sorted(boxes, key=lambda e: (e[0], e[1], e[2]))
+
+    n = len(boxes)
+    max_height = {(b[0], b[1]): b[2] for b in boxes}
+
+    for i in range(n):
+        box1_x, box1_y = boxes[i][0], boxes[i][1]
+        for j in range(i+1, n):
+            box2_x, box2_y, box2_z = boxes[j][0], boxes[j][1], boxes[j][2]
+            max_tmp = max_height[box2_x, box2_y]
+            if box1_x < box2_x and box1_y < box2_y:
+                max_tmp = max(max_tmp, max_height[box1_x, box1_y] + box2_z)
+            max_height[box2_x, box2_y] = max_tmp
+    return max([v for _, v in max_height.items()])
